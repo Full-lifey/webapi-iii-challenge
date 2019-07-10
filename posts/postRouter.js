904d1f5d6ec01ae14c8696cf704a36085693f05a
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-07-10 17:05:06 -0600

    PUT /api/posts/:id endpoint completed

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const Posts = require('./postDb.js');
+const userRouter = require('../users/userRouter.js');
 
 router.get('/', (req, res) => {
   Posts.get()
@@ -34,7 +35,14 @@ router.delete('/:id', validatePostId, (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {});
+router.put('/:id', validatePostId, validatePost, async (req, res) => {
+  try {
+    const post = await Posts.update(req.params.id, req.body);
+    res.status(200).json(await Posts.getById(req.params.id));
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to update post' });
+  }
+});
 
 // custom middleware
 
@@ -51,6 +59,14 @@ function validatePostId(req, res, next) {
     .catch(error => {
       res.status(500).json({ error: 'Server broken' });
     });
+}
+
+function validatePost(req, res, next) {
+  if (!req.body) {
+    res.status(400).json({ message: 'missing post data' });
+  } else if (!req.body.text) {
+    res.status(400).json({ message: 'missing required text field' });
+  } else next();
 }
 
 module.exports = router;
