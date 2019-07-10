06967da262e067af528309b5ad2d329adc88a9ae
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-07-10 13:50:18 -0600

    initial commit, tested GET on / and GET on /api/users

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
+const userRouter = require('./users/userRouter.js');
+
+server.use(logger);
+server.use(express.json());
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} from ${req.url}`);
 
-};
+  next();
+}
 
+server.use('/api/users', userRouter);
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,46 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
 
+const Users = require('./userDb.js');
+
 router.post('/', (req, res) => {
-
+  console.log('/api/users GET');
+  Users.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch();
 });
 
-router.post('/:id/posts', (req, res) => {
-
-});
+router.post('/:id/posts', (req, res) => {});
 
 router.get('/', (req, res) => {
-
+  Users.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch();
 });
 
 router.get('/:id', (req, res) => {
+  const { id } = req.params;
 
+  Users.getById(id);
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', (req, res) => {});
 
-});
+router.delete('/:id', (req, res) => {});
 
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put('/:id', (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+function validateUserId(req, res, next) {}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
