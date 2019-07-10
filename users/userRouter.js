201d2e5bbbcd32fbc8f66e449880d7b259127b7c
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-07-10 14:04:02 -0600

    validateUser middleware complete

--- users/userRouter.js
+++ users/userRouter.js
@@ -53,7 +53,13 @@ function validateUserId(req, res, next) {
     });
 }
 
-function validateUser(req, res, next) {}
+function validateUser(req, res, next) {
+  if (!req.body) {
+    res.status(400).json({ message: 'missing user data' });
+  } else if (!req.body.name) {
+    res.status(400).json({ message: 'missing required name field' });
+  } else next();
+}
 
 function validatePost(req, res, next) {}
 
